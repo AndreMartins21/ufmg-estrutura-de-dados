96f17d385d336dea56c2662e39667c729410cdd5
André Augusto Moreira Martins <64978311+AndreMartins21@users.noreply.github.com>
2024-11-11 22:33:19 -0300

    fix: Adiciona consumir_recursos no fibonacci

--- aula_pratica_3/src/fibonacci.c
+++ aula_pratica_3/src/fibonacci.c
@@ -1,5 +1,19 @@
+#include <math.h>  
+#include <stdio.h>
+
+
+
+void consumir_recursos_(){
+    double x = 1.0;
+    for (int i = 0; i < 1000000; i++) {
+        sin(x);  // Calcular o seno repetidamente
+    }
+}
+
 unsigned long long int fibonacci_recursivo(int posicao){
     if (posicao <= 1) return 1;
+
+    consumir_recursos_();
 
     return fibonacci_recursivo(posicao-1) + fibonacci_recursivo(posicao-2);
 }
