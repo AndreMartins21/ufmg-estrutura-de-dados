2f1af4bb9ea6108589c762b082289be880b5e5cd
André Augusto Moreira Martins <64978311+AndreMartins21@users.noreply.github.com>
2024-11-11 22:33:45 -0300

    fix: Adiciona consumir_recursos no fatorial

--- aula_pratica_3/src/fatorial.c
+++ aula_pratica_3/src/fatorial.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <math.h>  
 
+
+void consumir_recursos(){
+    double x = 1.0;
+    for (int i = 0; i < 1000000; i++) {
+        sin(x);  // Calcular o seno repetidamente
+    }
+}
 
 unsigned long long int fatorial_recursivo(int n){
     if (n<=0) return 1;
+    consumir_recursos();
 
     return n*fatorial_recursivo(n-1);
 }
